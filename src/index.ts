261a2cf08bd605163546d3db9ca4225045574ea9
Nick Thomas <nichoth@nichoth.com>
2024-04-26 21:13:18 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,105 @@
+import ts from 'monotonic-timestamp'
+import type { Implementation } from '@oddjs/odd/components/crypto/implementation'
+import { SignedMessage } from '@bicycle-codes/message'
 import { createDebug } from '@nichoth/debug'
+import { blake3 } from '@noble/hashes/blake3'
+import { DID, Identity, sign } from '@bicycle-codes/identity'
+import { toString } from 'uint8arrays/to-string'
+import stringify from 'json-canon'
+type KeyStore = Implementation['keystore']
 const debug = createDebug()
 
-export function example ():void {
-    debug('hello')
+export interface Metadata {
+    timestamp:number;
+    proof:string,
+    key:string,
+    seq:number;
+    lipmalink:string;
+    prev:string|null;
+    username:string;
+    author:DID;
 }
+
+export type SignedMetadata = SignedMessage<Metadata>
+
+/**
+ * `alt` should be an array of 'alt' strings, in the same order as
+ * the provided `mentions`.
+ */
+export interface Content {
+    text:string,
+    alt?:string[],
+    mentions?:string[]
+}
+
+export type SignedPost = { metadata:SignedMetadata, content:Content }
+export type EncryptedPost = { metadata:SignedMetadata, content:string }
+
+/**
+ * Create a new message by the given identity.
+ *
+ * @param user The identity that is creating the message
+ * @param keystore A keystore instance
+ * @param opts Message data
+ */
+export async function create (
+    user:Identity,
+    keystore:KeyStore,
+    opts:{
+        username:string,
+        content:Content,
+        seq:number,
+        prev:SignedPost|null|undefined,  // the hash of the previous message
+    }
+):Promise<SignedPost> {
+    const metadata:Partial<SignedMetadata> = {
+        timestamp: ts(),
+        proof: toString(blake3(stringify(opts.content)), 'base64pad'),
+        author: user.rootDID,
+        prev: '',
+        lipmalink: '',
+        username: user.username,
+        seq: (opts.prev?.metadata.seq || 0) + 1,
+    }
+
+    const str = stringify(metadata)
+    metadata.signature = toString(await sign(keystore, str), 'base64pad')
+    const key = toString(blake3(str))
+
+    return {
+        metadata: { ...metadata, key } as SignedMetadata,
+        content: opts.content
+    }
+}
+
+/**
+ * @see {@link https://github.com/AljoschaMeyer/bamboo?tab=readme-ov-file#concepts-and-properties bamboo docs}
+ *
+ * > Conceptually, an entry in the log is a tuple of
+ *   - tag, either a zero byte (0x00) to indicate a regular log entry, or a one
+ *     byte (0x01) to indicate an end-of-log marker. No other values are valid.
+ *     This can serve as an extension point: Future protocols that should be
+ *     compatible with bamboo can use different tag values to introduce new
+ *     functionality, for example signing primitives other than ed25519.
+ *   - author -- the public key
+ *   - log ID -- a 64 bit integer serves to distinguish different logs by the
+ *     same author, encoded as a canonical VarU64
+ *   - sequence number of the entry (i.e. the offset in the log)
+ *   - the backlink, a cryptographically secure hash of the previous entry in
+ *     the log
+ *   - the lipmaalink, a cryptographically secure hash of some older entry in
+ *     the log, chosen such that there are short paths between any pair
+ *     of entries
+ *   - the hash of the actual payload of the entry
+ *   - the size of the payload in bytes
+ *   - a boolean that indicates whether this is a regular entry or an
+ *     end-of-log marker
+ *   - the digital signature of all the previous data, issued with the
+ *     log's public key
+ */
+
+/**
+ * Need to figure out the limpaa link.
+ * Given a new entry and an existing log,
+ * what is the lipma link in the new entry?
+ */
--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,41 @@
 import { test } from '@bicycle-codes/tapzero'
-import { example } from '../src/index.js'
+import { components } from '@ssc-half-light/node-components'
+import * as odd from '@oddjs/odd'
+import { create as createMsg, SignedPost } from '../src/index.js'
+import { create as createID, Identity } from '@bicycle-codes/identity'
 
-test('example', async t => {
-    t.ok('ok', 'should be an example')
-    example()
+let program:odd.Program
+let alice:Identity
+
+test('setup', async t => {
+    program = await odd.assemble({
+        namespace: { creator: 'test', name: 'testing' },
+        debug: false
+    }, components)
+
+    alice = await createID(program.components.crypto, {
+        humanName: 'alice',
+        humanReadableDeviceName: 'computer'
+    })
+
+    t.ok(program, 'create a program')
+    t.ok(alice, 'create an identity')
+})
+
+let post:SignedPost
+test('create a new message', async t => {
+    post = await createMsg(
+        alice,
+        program.components.crypto.keystore,
+        {
+            username: 'alice',
+            seq: 1,
+            prev: null,
+            content: {
+                text: 'hello'
+            }
+        }
+    )
+
+    t.ok(post, 'should create a message')
 })
